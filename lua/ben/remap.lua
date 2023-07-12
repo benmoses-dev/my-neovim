cb2749bed9b143f3b3476286b1a94285cdf2f250
Ben M <ben@mosesweb.co.uk>
2023-07-12 22:52:48 +0100

    alter refactor and search keymaps

--- lua/ben/remap.lua
+++ lua/ben/remap.lua
@@ -32,10 +32,10 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.keymap.set("n", "Q", "<nop>")
 
 -- format the buffer
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set({"n", "v"}, "Q", "gq")
 
 vim.keymap.set("n", "<leader>h", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>cprev<CR>zz")
@@ -43,7 +43,8 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>l", "<cmd>lprev<CR>zz")
 
 -- search and replace all occurrences of the current word
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<\>//gc<Left><Left><Left><Left><Left><Left>]])
 
 -- make current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
