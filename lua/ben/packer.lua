0d656a90d340406401ee8e090e615f1526ca36b6
Ben M <ben@mosesweb.co.uk>
2023-07-14 21:18:36 +0100

    add kanagawa and tokyonight themes

--- lua/ben/packer.lua
+++ lua/ben/packer.lua
@@ -7,22 +7,28 @@ return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
+    -- Find files and content across project
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
+    -- Syntax highlighting
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
 	use('nvim-treesitter/playground')
 
+    -- Bookmark commonly used files
 	use('ThePrimeagen/harpoon')
 
+    -- Complete undo history including branches
 	use('mbbill/undotree')
 
+    -- Git integration
 	use('tpope/vim-fugitive')
 
+    -- Language server support
 	use {
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v2.x',
@@ -39,7 +45,12 @@ return require('packer').startup(function(use)
 		}
 	}
 
+    -- Rust utilities
     use('simrat39/rust-tools.nvim')
+
+    -- Themes
+    use('folke/tokyonight.nvim')
+    use('rebelot/kanagawa.nvim')
 
 end)
 
