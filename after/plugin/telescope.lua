473cab59469c7601e17db63c51433d006bc08879
Ben M <ben@mosesweb.co.uk>
2023-07-19 23:44:02 +0100

    feat: add project commits and branches to telescope bindings, change pg to pd for project (git) diff

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,24 @@
 local builtin = require('telescope.builtin')
 
+-- Project files
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 
-vim.keymap.set('n', '<leader>pg', builtin.git_status, {})
+-- Project git diff
+vim.keymap.set('n', '<leader>pd', builtin.git_status, {})
 
+-- Project search
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
+-- Project live search
 vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
+
+-- Telescope
+vim.keymap.set('n', '<leader>pp', builtin.planets, {})
+
+-- Project commits
+vim.keymap.set('n', '<leader>pc', builtin.git_commits, {})
+
+-- Project branches
+vim.keymap.set('n', '<leader>pb', builtin.git_branches, {})
