edaa4c7004a2193937d4982b6848e78d18337d0b
Ben M <ben@mosesweb.co.uk>
2023-07-24 20:26:34 +0100

    get copilot working, improve snippet engine

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,7 +5,7 @@ lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({ buffer = bufnr })
 
     -- Modify default keymaps below
-    local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
     -- Use telescope to view references
     vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = true })
@@ -36,8 +36,24 @@ lsp.setup()
 -- You need to setup `cmp` after lsp-zero
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
+local luasnip = require('luasnip')
+
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
     mapping = {
         -- Use p and n to select previous and next item in the list
         ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
@@ -49,34 +65,73 @@ cmp.setup({
         -- Ctrl+Space to trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
 
+        ["<CR>"] = cmp.mapping({
+            i = function(fallback)
+                if cmp.visible() and cmp.get_active_entry() then
+                    cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                else
+                    fallback()
+                end
+            end,
+            s = cmp.mapping.confirm({ select = true }),
+            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        }),
+
         -- Abort the current completion and hide the menu
         ['<C-e>'] = cmp.mapping.abort(),
 
         -- Disable up and down arrows, tab and shift+tab and enter (use n, p and y instead)
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            cmp.close()
-            fallback()
-        end, {'i', 's'}),
-
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            cmp.close()
-            fallback()
-        end, {'i', 's'}),
-
         ['<Up>'] = cmp.mapping(function(fallback)
             cmp.close()
             fallback()
-        end, {'i', 's'}),
+        end, { 'i', 's' }),
 
         ['<Down>'] = cmp.mapping(function(fallback)
             cmp.close()
             fallback()
-        end, {'i', 's'}),
+        end, { 'i', 's' }),
 
-        ['<CR>'] = cmp.mapping(function(fallback)
-            cmp.close()
-            fallback()
-        end, {'i', 's'}),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                local entry = cmp.get_selected_entry()
+	            if not entry then
+	                cmp.close()
+                    fallback()
+	            else
+	                cmp.confirm()
+                end
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    },
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp', keyword_length = 1 },
+        { name = 'buffer',   keyword_length = 3 },
+        { name = 'luasnip',  keyword_length = 2 },
     }
 })
 
+-- Use cmdline & path source for ':'.
+cmp.setup.cmdline(':', {
+    completion = { autocomplete = false },
+    sources = cmp.config.sources({
+        { name = 'path' },
+        { name = 'cmdline' },
+    })
+})
