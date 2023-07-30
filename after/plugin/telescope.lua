0f85810f127540ae00ecfa39ff66b30980bb9747
Ben M <ben@mosesweb.co.uk>
2023-07-30 21:19:29 +0100

    fix: stop previewer trying to show binary files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,16 +1,57 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+local previewers = require("telescope.previewers")
+local Job = require("plenary.job")
 
 -- Telescope configuration
 
+-- Do not preview any file larger than 20K
+-- local new_maker = function(filepath, bufnr, opts)
+--     opts = opts or {}
+--
+--     filepath = vim.fn.expand(filepath)
+--     vim.loop.fs_stat(filepath, function(_, stat)
+--         if not stat then return end
+--         if stat.size > 20000 then
+--             return
+--         else
+--             previewers.buffer_previewer_maker(filepath, bufnr, opts)
+--         end
+--     end)
+-- end
+
+-- Do not preview binary files
+local new_maker = function(filepath, bufnr, opts)
+    filepath = vim.fn.expand(filepath)
+    Job:new({
+        command = "file",
+        args = { "--mime-type", "-b", filepath },
+        on_exit = function(j)
+            local mime_type = vim.split(j:result()[1], "/")[1]
+            if mime_type == "text" then
+                previewers.buffer_previewer_maker(filepath, bufnr, opts)
+            else
+                -- maybe we want to write something to the buffer here
+                vim.schedule(function()
+                    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+                end)
+            end
+        end
+    }):sync()
+end
+
 telescope.setup({
-	pickers = {
-		find_files = {
+    defaults = {
+        buffer_previewer_maker = new_maker,
+    },
+    pickers = {
+        find_files = {
             -- use find_files to show all files (including ignored and hidden files)
             -- use git_files instead to show all tracked files (respects .gitignore, shows only tracked dotfiles)
-			find_command = { "fdfind", "--type", "f", "--strip-cwd-prefix", "--hidden", "--no-ignore", "--exclude", "**/.git/*" },
-		},
-	},
+            find_command = { "fdfind", "--type", "f", "--strip-cwd-prefix", "--hidden", "--no-ignore", "--exclude",
+                "**/.git/*" },
+        },
+    },
 })
 
 -- Standard Keymaps
@@ -53,4 +94,3 @@ vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 
 -- View planets...
 vim.keymap.set('n', '<leader>pp', builtin.planets, {})
-
